e1d719e2c7a3c393a93d5142df489366782d58b1
madhurisha24 <madhurishakarthikeyan@gmail.com>
2026-05-21 14:36:04 +0530

    Add files via upload

---
+++ digital clock/digital.js
@@ -0,0 +1,23 @@
+function updateClock(){
+      let now = new Date();
+
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let seconds = now.getSeconds();
+
+      let ampm = hours >= 12 ? "PM" : "AM";
+
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+
+      hours = hours.toString().padStart(2,'0');
+      minutes = minutes.toString().padStart(2,'0');
+      seconds = seconds.toString().padStart(2,'0');
+
+      document.getElementById("clock").innerHTML =
+      `${hours}:${minutes}:${seconds} ${ampm}`;
+    }
+
+    setInterval(updateClock,1000);
+
+    updateClock();
